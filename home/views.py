477ee7f6624b4eba582dd47cdcb1853f8fd77fd2
KieranGuelfi <95836976+KieranGuelfi@users.noreply.github.com>
2022-10-11 22:41:44 +0800

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def simpleview(request):
+    return HttpResponse('This is a simple view.')
